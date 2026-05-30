1ff55aa461a379c9147971f9686cfbef5e19e020
Git-vip8 <shl888shl888@gmail.com>
2026-05-30 21:06:06 +0800

    Add files via upload

--- 节点配置转换为字符串.py
+++ 节点配置转换为字符串.py
@@ -12,7 +12,7 @@ def encode_subscription(links):
 if __name__ == "__main__":
     # 在这里填入你的节点链接
     nodes = [
-        "hysteria2://Tz123456@43.131.225.218:443?sni=www.google.com&insecure=1#Tencent-Hy2",
+        "hysteria2://Tz123456@43.131.225.218:1992?sni=www.google.com&insecure=1#Tencent-Hy2",
         "vless://06f1805c-d31a-42a2-a0e6-a4ad476cd89f@43.131.225.218:8443?type=tcp&security=reality&pbk=0qNolE95MxKMmT1Ymlj5bhxEH0TKXC0N0e0M9P8T81E&fp=chrome&sni=www.apple.com&sid=717f6e79b0fb00cc&spx=%2F#Tencent-VLESS"
     ]
     
