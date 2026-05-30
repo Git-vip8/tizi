b39cbbdbe70473fb3d353db109430288794e3cfa
Git-vip8 <shl888shl888@gmail.com>
2026-05-30 17:38:32 +0800

    Add files via upload

--- bushu.sh
+++ bushu.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
-# 1. 复制配置文件到指定目录
+# 1. 创建证书目录并生成自签名证书
+mkdir -p /etc/sing-box/certs
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
+  -keyout /etc/sing-box/certs/private.key \
+  -out /etc/sing-box/certs/cert.pem \
+  -subj "/CN=www.google.com"
+
+# 2. 复制配置文件到指定目录
 cp ./hy2.json /etc/sing-box/hy2.json
 cp ./vless-reality.json /etc/sing-box/vless-reality.json
 
-# 2. 用指定的配置文件启动 sing-box，后台运行
+# 3. 用指定的配置文件启动 sing-box，后台运行
 sing-box run -c /etc/sing-box/hy2.json &
 sing-box run -c /etc/sing-box/vless-reality.json &
 
